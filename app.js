bf267022f6b1902cf0e80c54564f8ad484cbc570
rkiel <cygnus@pobox.com>
2015-09-06 21:21:05 -0400

    1.4

--- app.js
+++ app.js
@@ -3,13 +3,23 @@
 var app = angular.module('store', []);
 
 app.controller('StoreController', function() {
-  this.product = gem;
+  this.products = gems;
 });
 
-var gem = {
+var gems = [
+{
   name: 'Dodecahedron',
   price: 2.95,
-  description: 'blah blah'
-};
+  description: 'blah blah',
+  canPurchase: false,
+  soldOut: true
+}, {
+  name: 'Pentagonal Gem',
+  price: 5.95,
+  description: 'blah blah',
+  canPurchase: false,
+  soldOut: false
+}
+];
 
 })();
